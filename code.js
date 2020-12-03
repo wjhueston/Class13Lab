2598cbd802fdcc18d59fa75f3faacf80865d5f4e
wjhueston <wjhueston@gmail.com>
2020-12-02 20:32:00 -0600

    Added a table with all previous entries, plus two sort functionalities

--- code.js
+++ code.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
     $("table").hide()
     //Events
     $("form").submit(Objection)
-    //Functions
+    //Function
+    var studentArray = []
     function Objection(event){
         event.preventDefault()
         var studentGrade={
@@ -11,6 +12,7 @@ $(document).ready(function () {
             pointsEarned: $("#pointsEarned").val(),
             pointsPossible: $("#pointsPossible").val()
         }
+
         $("#firstOutput").text(studentGrade.firstName)
         $("#lastOutput").text(studentGrade.lastName)
         var letterGrade
@@ -53,7 +55,114 @@ $(document).ready(function () {
             letterGrade = "A"
         }
         $("#letterScore").text(letterGrade)
+
+        //New student objects here
+        var studentArrayObj ={
+            first: studentGrade.firstName,
+            last: studentGrade.lastName,
+            percentScore: percentGrade,
+            letterScore: letterGrade
+        }
+
+        studentArray.push(studentArrayObj);
+
+
+        var arrayTable = document.getElementById("arrayTable")
+
+        $("#arrayTable tbody").children().remove()
+        var header = arrayTable.insertRow(0)
+        var headerCell1 = header.insertCell(0)
+        var headerCell2 = header.insertCell(1)
+        var headerCell3 = header.insertCell(2)
+        var headerCell4 = header.insertCell(3)
+        headerCell1.innerHTML = "First Name"
+        headerCell2.innerHTML = "Last Name"
+        headerCell3.innerHTML = "Percent Score"
+        headerCell4.innerHTML = "Letter Grade"
+        studentArray.forEach(arrayReadout)
+        function arrayReadout(item, index){
+            var row = arrayTable.insertRow(index+1)
+            var cell1 = row.insertCell(0)
+            var cell2 = row.insertCell(1)
+            var cell3 = row.insertCell(2)
+            var cell4 = row.insertCell(3)
+            cell1.innerHTML = item.first
+            cell2.innerHTML = item.last
+            cell3.innerHTML = item.percentScore.toFixed(2) + "%"
+            cell4.innerHTML = item.letterScore
+        }
         $("table").show()
+        $("#sortbyName").show();
+        $("#sortbyPercent").show();
+
+
     }
+        $("#sortbyName").click(SortByName)
+
+        function SortByName(){
+            function NameSort(student1, student2){
+                if(student1.last < student2.last) return -1;
+                if(student1.last > student2.last) return 1;
+                return 0;
+            }
+            studentArray.sort(NameSort);
+            var arrayTable = document.getElementById("arrayTable")
+
+            $("#arrayTable tbody").children().remove()
+            var header = arrayTable.insertRow(0)
+            var headerCell1 = header.insertCell(0)
+            var headerCell2 = header.insertCell(1)
+            var headerCell3 = header.insertCell(2)
+            var headerCell4 = header.insertCell(3)
+            headerCell1.innerHTML = "First Name"
+            headerCell2.innerHTML = "Last Name"
+            headerCell3.innerHTML = "Percent Score"
+            headerCell4.innerHTML = "Letter Grade"
+            studentArray.forEach(arrayReadout)
+            function arrayReadout(item, index){
+                var row = arrayTable.insertRow(index+1)
+                var cell1 = row.insertCell(0)
+                var cell2 = row.insertCell(1)
+                var cell3 = row.insertCell(2)
+                var cell4 = row.insertCell(3)
+                cell1.innerHTML = item.first
+                cell2.innerHTML = item.last
+                cell3.innerHTML = item.percentScore.toFixed(2) + "%"
+                cell4.innerHTML = item.letterScore
+            }
+        }
+        $("#sortbyPercent").click(SortByPercent)
+        function SortByPercent(){
+            function PercentSort(student1, student2){
+                if(student1.percentScore < student2.percentScore) return -1;
+                if(student1.percentScore > student2.percentScore) return 1;
+                return 0
+            }
+            studentArray.sort(PercentSort)
+            var arrayTable = document.getElementById("arrayTable")
+
+            $("#arrayTable tbody").children().remove()
+            var header = arrayTable.insertRow(0)
+            var headerCell1 = header.insertCell(0)
+            var headerCell2 = header.insertCell(1)
+            var headerCell3 = header.insertCell(2)
+            var headerCell4 = header.insertCell(3)
+            headerCell1.innerHTML = "First Name"
+            headerCell2.innerHTML = "Last Name"
+            headerCell3.innerHTML = "Percent Score"
+            headerCell4.innerHTML = "Letter Grade"
+            studentArray.forEach(arrayReadout)
+            function arrayReadout(item, index){
+                var row = arrayTable.insertRow(index+1)
+                var cell1 = row.insertCell(0)
+                var cell2 = row.insertCell(1)
+                var cell3 = row.insertCell(2)
+                var cell4 = row.insertCell(3)
+                cell1.innerHTML = item.first
+                cell2.innerHTML = item.last
+                cell3.innerHTML = item.percentScore.toFixed(2) + "%"
+                cell4.innerHTML = item.letterScore
+            }
+        }
 }
 )
